a634d300f1785af2a42fe7c2878f5967ea1992f3
Maxim <93578199+maximcuynat@users.noreply.github.com>
2026-02-09 00:40:20 +0100

    Correction index.php

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', str_replace("index.php", "stages", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+define('URL', str_replace("index.php", "home", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
 
 require_once('controllers/Router.php');
 $router = new Router();
